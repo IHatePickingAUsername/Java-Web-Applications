1707287ffa5a16cb019ebcdb6a83c1c4da8b1bd6
Admin <Admin@RJENS>
2017-03-27 20:04:13 -0400

    First submission

---
+++ src/java/LoginServlet.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class LoginServlet extends HttpServlet{
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (username == "jsmith@toba.com" && password == "letmein") {
+            String url = "/account_activity.html";
+            getServletContext().getRequestDispatcher(url).forward(request, response);
+        } else {
+            String url = "/login_failure.html";
+            getServletContext().getRequestDispatcher(url).forward(request, response);
+        }
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
